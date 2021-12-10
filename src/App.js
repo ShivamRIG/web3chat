bb17c4629d4c2040badb90bf2f4f23604787056a
Shivam <shivamsingh8461@outlook.com>
2021-12-10 16:00:08 +0530

    front end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {AppBar,Typography,Toolbar,IconButton,TextField,Button} from '@mui/material'
+
 
 function App() {
+  
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Web Dapp Chat
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <TextField label="Enter the text" id="fullWidth" sx={{marginTop:100,paddingRight:2,width:1500}} />
+      <Button variant='outlined'sx={{marginTop:100,height:55}} >Send</Button>
+  
+   
     </div>
   );
 }
